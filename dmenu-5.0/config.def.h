3c42e726a8fd77bf28233656a511668ad82b58c6
_N0x <tf@n0x.io>
2021-10-17 14:56:40 +0200

    Added alpha patch for dmenu and minor config tweaks

--- dmenu-5.0/config.def.h
+++ dmenu-5.0/config.def.h
@@ -3,7 +3,8 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 550;                    /* minimum width when centered */
+static int min_width = 600;                    /* minimum width when centered */
+static const unsigned int alpha = 0xe0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"MesloLGS NF:size=11"
@@ -18,6 +19,13 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 12;
 /* -h option; minimum height of a menu line */
@@ -31,4 +39,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 3;
