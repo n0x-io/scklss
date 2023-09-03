162de47245f83116a3dcaf78c07d0479dc419b3a
_N0x <tf@n0x.io>
2023-09-03 19:50:01 +0200

    Upgraded slock to 1.5

---
+++ slock-1.5/config.def.h
@@ -0,0 +1,24 @@
+/* user and group to drop privileges to */
+static const char *user = "n0x";
+static const char *group = "users";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time in seconds to cancel lock with mouse movement */
+static const int timetocancel = 4;
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=30;
+/*Enable Pixelation*/
+#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=10;
